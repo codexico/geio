dab16e4a7c4928ce8667577e268aeabd9d50a22d
codexico <codexico@gmail.com>
2010-04-04 19:36:36 -0300

    ~pdf

--- app/controllers/cupom_promocionais_controller.php
+++ app/controllers/cupom_promocionais_controller.php
@@ -70,5 +70,58 @@ class CupomPromocionaisController extends AppController {
             $this->Session->setFlash(__('The CupomPromocional could not be deleted. Please, try again.', true));
             $this->redirect(array('action' => 'index'));
     }
+
+
+
+    function cupomPdf() {
+//debug($content_for_layout);
+        /*
+        if (!$id) {
+            $this->Session->setFlash('Sorry, there was no property ID submitted.');
+            $this->redirect(array('action'=>'index'), null, true);
+        }
+         *
+        */
+        //Configure::write('debug',0); // Otherwise we cannot use this method while developing
+
+        //$id = intval($id);
+
+        //$property = $this->__view($id); // here the data is pulled from the database and set for the view
+        //$property = "testestse";
+        /*
+        if (empty($property)) {
+            $this->Session->setFlash('Sorry, there is no property with the submitted ID.');
+            $this->redirect(array('action'=>'index'), null, true);
+        }
+        */
+        $this->layout = 'pdf'; //this will use the pdf.ctp layout
+        $teste = "tttestset";
+        $this->set(compact('teste'));
+        /*
+        $teste = "tttestset";
+        $this->set(compact('teste'));
+        $rendered = $this->render();
+        $fileext = 'pdf'; // optional: $this->params['url']['ext'];
+        $filepath = TMP.'rendered'.DS;
+        debug($filepath);
+        $filename = md5(String::uuid()).'.'.$fileext;
+        file_put_contents($filepath.$filename, $rendered); // php5 func
+        $this->view = 'Media';
+        $params = array(
+                'id' => $filename.'x',
+                'path' => $filepath,
+                'extension' => $fileext,
+                'download' => true, // force download
+                'name' => low($this->name).'-export' // fancy name
+        );
+        $this->set($params);
+         *
+         */
+
+        $this->render();
+            //$this->redirect(array('controller'=>'Consumidores', 'action' => 'pesquisar'));
+    }
+
+
 }
 ?>
