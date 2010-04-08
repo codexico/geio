955cf712fe459695f47d09a380ea7b84c648ef29
codexico <codexico@gmail.com>
2010-04-08 02:51:24 -0300

    resumo diario fat model

--- app/controllers/resumo_diarios_controller.php
+++ app/controllers/resumo_diarios_controller.php
@@ -1,64 +1,31 @@
 <?php
+/**
+ * @property Troca $Troca
+ * @property CupomPromocional $CupomPromocional
+ * @property ResumoDiario $ResumoDiario
+ * @property DiaTroca $DiaTroca
+ * @property Consumidor $Consumidor
+ * @property TrocasDia $TrocasDia
+ */
 class ResumoDiariosController extends AppController {
 
-	var $name = 'ResumoDiarios';
-	var $helpers = array('Html', 'Form');
+    var $name = 'ResumoDiarios';
 
-	function index() {
-		$this->ResumoDiario->recursive = 0;
-		$this->set('resumoDiarios', $this->paginate());
-	}
+    var $helpers = array('Html', 'Form', 'Javascript', 'CakePtbr.Formatacao');
 
-//	function view($id = null) {
-//		if (!$id) {
-//			$this->Session->setFlash(__('Invalid ResumoDiario', true));
-//			$this->redirect(array('action' => 'index'));
-//		}
-//		$this->set('resumoDiario', $this->ResumoDiario->read(null, $id));
-//	}
-//
-//	function add() {
-//		if (!empty($this->data)) {
-//			$this->ResumoDiario->create();
-//			if ($this->ResumoDiario->save($this->data)) {
-//				$this->Session->setFlash(__('The ResumoDiario has been saved', true));
-//				$this->redirect(array('action' => 'index'));
-//			} else {
-//				$this->Session->setFlash(__('The ResumoDiario could not be saved. Please, try again.', true));
-//			}
-//		}
-//	}
-//
-//	function edit($id = null) {
-//		if (!$id && empty($this->data)) {
-//			$this->Session->setFlash(__('Invalid ResumoDiario', true));
-//			$this->redirect(array('action' => 'index'));
-//		}
-//		if (!empty($this->data)) {
-//			if ($this->ResumoDiario->save($this->data)) {
-//				$this->Session->setFlash(__('The ResumoDiario has been saved', true));
-//				$this->redirect(array('action' => 'index'));
-//			} else {
-//				$this->Session->setFlash(__('The ResumoDiario could not be saved. Please, try again.', true));
-//			}
-//		}
-//		if (empty($this->data)) {
-//			$this->data = $this->ResumoDiario->read(null, $id);
-//		}
-//	}
-//
-//	function delete($id = null) {
-//		if (!$id) {
-//			$this->Session->setFlash(__('Invalid id for ResumoDiario', true));
-//			$this->redirect(array('action' => 'index'));
-//		}
-//		if ($this->ResumoDiario->del($id)) {
-//			$this->Session->setFlash(__('ResumoDiario deleted', true));
-//			$this->redirect(array('action' => 'index'));
-//		}
-//		$this->Session->setFlash(__('The ResumoDiario could not be deleted. Please, try again.', true));
-//		$this->redirect(array('action' => 'index'));
-//	}
+    //var $uses = array('Troca','CupomPromocional','CupomFiscal','DiaTroca','Consumidor','TrocasDia','ResumoDiario');
+
+    function beforeFilter() {
+        parent::beforeFilter();
+    }
+
+    function index() {
+        
+        $this->ResumoDiario->atualizar();
+
+        $this->ResumoDiario->recursive = 0;
+        $this->set('resumoDiarios', $this->paginate());
+    }
 
 }
 ?>
--- app/models/dia_troca.php
+++ app/models/dia_troca.php
@@ -1,8 +1,19 @@
 <?php
+/**
+ * @property DiaTroca $DiaTroca
+ */
 class DiaTroca extends AppModel {
 
     var $name = 'DiaTroca';
 
     //var $useTable = false;
+
+    function getMaxDia(){
+        $maxDia = $this->field('dia', array(), 'dia DESC');//debug($maxDia);
+        if(!$maxDia) {
+            $maxDia =  date('Y-m-d', strtotime(Configure::read('Campanha.Inicio') . " -21 days"));//inicio da campanha
+        }//debug($maxDia);
+        return $maxDia;
+    }
 }
 ?>
